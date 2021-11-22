b5a15da5a4b40bf37b242e6e6397033eefc97867
Victor200223 <91858414+Victor200223@users.noreply.github.com>
2021-11-21 23:58:02 -0500

    Create Comentario.cs

---
+++ Reto 4 Restaurante/Models/Comentario.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reto_4_Restaurante.Models
+{
+    public class Comentario
+    {
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public string descripcion { get; set; }
+    }
+}
